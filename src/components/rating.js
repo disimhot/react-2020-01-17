b8ab2076876f7b28231f04e963d8bafcc4d6b12f
Ефимова Елена Сергеевна <ElSEfimova@vimpelcom.ru>
2020-01-23 15:11:26 +0300

    rating count

--- src/components/rating.js
+++ src/components/rating.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, {useMemo} from 'react'
 import {Rate} from 'antd'
 
 function Rating(props) {
+  const memoRating = useMemo(() => {
+    return Math.round(
+      props.reviews.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.rating,
+        0
+      ) / props.reviews.length
+    )
+  }, [props.reviews])
+
   return (
     <div>
       <h4>Средний рейтинг</h4>
-      <Rate count={starsAmount} value={props.rating} disabled />
+      <Rate count={starsAmount} value={memoRating} disabled />
     </div>
   )
 }
--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -1,23 +1,13 @@
-import React, {useMemo} from 'react'
+import React from 'react'
 import Menu from './menu'
 import Reviews from './reviews'
 import Rating from './rating'
 
 function Restaurant(props) {
-  const memoRating = useMemo(() => {
-    return Math.round(
-      props.restaurant.reviews.reduce(
-        (accumulator, currentValue) => accumulator + currentValue.rating,
-        0
-      ) / props.restaurant.reviews.length
-    )
-  }, [props.restaurant.reviews])
-
   return (
     <div>
-      Rest COMPs
       <Menu restaurant={props.restaurant} />
-      <Rating rating={memoRating} />
+      <Rating reviews={props.restaurant.reviews} />
       <Reviews reviews={props.restaurant.reviews} />
     </div>
   )
