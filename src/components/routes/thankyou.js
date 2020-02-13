f0f84907266f7773e0b860d1f236a54b66cb1990
Elena Efimova <ees.elena@gmail.com>
2020-02-13 22:26:21 +0300

    .

---
+++ src/components/routes/thankyou.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Result, Icon} from 'antd'
+
+function ThankYouPage() {
+  return (
+    <Result
+      icon={<Icon type="smile" theme="twoTone" />}
+      title="Thank you for your order"
+    />
+  )
+}
+
+export default ThankYouPage
